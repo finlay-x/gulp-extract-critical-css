9b759cd2533197c7bb1dcc9091ce5370ea9496f6
finlay-x <67915426+finlay-x@users.noreply.github.com>
2023-03-24 06:52:13 +0000

    Typescript type declaration file update

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,15 @@
 declare module 'gulp-extract-critical-css' {
     import { Transform } from 'stream';
-    import Vinyl from 'vinyl';
-  
-    interface GulpExtractCriticalCssOptions {}
-  
-    function gulpExtractCriticalCss(
-      options?: GulpExtractCriticalCssOptions,
-    ): Transform;
-  
+    
+    interface Options {
+      inlinePath?: string;
+      inlineCritical?: boolean;
+      modifySource?: boolean;
+    }
+    
+    function extractCriticalCss(file: Vinyl, contents: string): { contents: string, criticalCss: string };
+    
+    function gulpExtractCriticalCss(options?: Options): Transform;
+    
     export = gulpExtractCriticalCss;
-}
+  }
